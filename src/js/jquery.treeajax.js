7981b07d57b3cca0e7bc9f6d0b06858e22ea55f0
Valerio Galano <v.galano@daredevel.com>
2011-12-11 11:30:49 +0100

    Ajax manipulation support

--- src/js/jquery.treeajax.js
+++ src/js/jquery.treeajax.js
@@ -18,13 +18,26 @@ $.widget("daredevel.treeajax", {
 
         var t = this;
 
+        // initialize ajax nodes
+        if (this.options.dataSourceUrl) {
+            t._lazyInit();
+        }
+
         // bind lazy loading on expand event
-        if (this.options.lazyLoadingUrl) {
+        if (this.options.dataSourceUrl) {
             this.element.bind("treeexpand", function(event, element) {
                 if ($(element).find('ul').length) {
                     return;
                 }
                 t._lazyLoad($(element));
+            });
+        }
+
+        // bind edit on drop event
+        if (this.options.dataEditUrl) {
+            this.element.bind("treemove", function(event, element) {
+                /** test if parent is not changed */
+                t._notifyMove($(element));
             });
         }
 
@@ -44,22 +57,47 @@ $.widget("daredevel.treeajax", {
      */
     _initializeNode: function(li) {
 
-        var t = this;
+//        var t = this;
 
     },
 
     /**
+     * Fetch tree via ajax
+     */
+    _lazyInit: function() {
+        var t = this;
+
+        $.ajax({
+            url: this.options.dataSourceUrl,
+            dataType:'json',
+            beforeSend: function() {
+                //alert('beforeSend');
+            },
+            complete: function() {
+                //alert('beforeSend');
+            },
+            data: {
+            },
+            success: function(data) {
+                $.each(data.nodes, function(key, value) {
+                    t.options.core.addNode(value);
+                });
+            }
+        });
+    },
+
+    /**
      * Fetch node's children via ajax
-     * 
+     *
      * @private
      *
      * @param parentLi node of which we want children
      */
     _lazyLoad: function(parentLi) {
         var t = this;
-        
+
         $.ajax({
-            url: this.options.lazyLoadingUrl,
+            url: this.options.dataSourceUrl,
             dataType:'json',
             data: {
                 node: parentLi.attr('id')
@@ -74,14 +112,75 @@ $.widget("daredevel.treeajax", {
     },
 
     /**
+     * Send move operation data via ajax
+     *
+     * @param li
+     */
+    _notifyMove: function(li) {
+        var t = this;
+
+        var parentLi = t.options.core.parentNode(li);
+
+        $.ajax({
+            url: this.options.dataEditUrl,
+            dataType:'json',
+            data: {
+                node: li.attr('id'),
+                operation: 'move',
+                parent: parentLi.attr('id')
+            },
+            beforeSend: function() {
+                alert('beforeSend');
+            },
+            success: function(data) {
+                alert('ok');
+            }
+        });
+    },
+
+    /**
      * Default options values
      */
     options: {
 
-        editUrl: '',
+        /**
+         * Defines url to request when tree modify operations happens.
+         *
+         * Server should return a null value if OK or an object like following:
+         * {
+         *     error: "there was an error saving ..."
+         * }
+         * The error string will be shown to user.
+         */
+        dataEditUrl: '',
 
-        lazyLoadingUrl: ''
-        
+        /**
+         * Defines url to request to get tree nodes for lazy loading.
+         *
+         * Server should return an object like following:
+         * {
+         *    "nodes": [
+         *       {
+         *           "span": {
+         *               "html": "Ajax node 1"
+         *           },
+         *           "li": {
+         *               "class": "collapsed"
+         *           }
+         *       },
+         *       {
+         *           "span": {
+         *               "html": "Ajax node 2"
+         *           },
+         *           "li": {
+         *               "class": "collapsed"
+         *           }
+         *       }
+         *    ]
+         * }
+         */
+        dataSourceUrl: ''
+
     }
 
 });
