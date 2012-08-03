90c206e7c9063acacdc498eed8d92f336c79497f
Valerio Galano <v.galano@daredevel.com>
2012-08-03 11:28:55 +0200

    Update features list

--- minify.sh
+++ minify.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 if [ -e jquery.tree.js ]; then rm jquery.tree.js; fi
-cat ./src/js/*.js > jquery.tree.js
-yui-compressor jquery.tree.js -o jquery.tree.min.js
+cat ./src/js/*.js > ./src/jquery.tree.js
+yui-compressor ./src/jquery.tree.js -o ./minified/jquery.tree.min.js
 
 if [ -e jquery.tree.css ]; then rm jquery.tree.css ; fi
-cat ./src/css/*.css > jquery.tree.css
-yui-compressor jquery.tree.css -o jquery.tree.min.css
+cat ./src/css/*.css > ./src/jquery.tree.css
+yui-compressor ./src/jquery.tree.css -o ./minified/jquery.tree.min.css
+
+if [ ! -e ./minified/images ]; then mkdir ./minified/images; fi
+cp ./src/images/*.gif ./minified/images/
