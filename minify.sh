add8ee7c3c8d5dced55aaf1772f98991faf8d735
Valerio Galano <v.galano@daredevel.com>
2012-08-06 12:27:36 +0200

    Prepare structure for uncompressed and minified versions

--- minify.sh
+++ minify.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+if [ ! -e ./uncompressed ]; then mkdir ./uncompressed; fi
+if [ ! -e ./minified ]; then mkdir ./minified; fi
+
 if [ -e jquery.tree.js ]; then rm jquery.tree.js; fi
-cat ./src/js/*.js > ./src/jquery.tree.js
-yui-compressor ./src/jquery.tree.js -o ./minified/jquery.tree.min.js
+cat ./src/js/*.js > ./uncompressed/jquery.tree.js
+yui-compressor ./uncompressed/jquery.tree.js -o ./minified/jquery.tree.min.js
 
 if [ -e jquery.tree.css ]; then rm jquery.tree.css ; fi
-cat ./src/css/*.css > ./src/jquery.tree.css
-yui-compressor ./src/jquery.tree.css -o ./minified/jquery.tree.min.css
+cat ./src/css/*.css > ./uncompressed/jquery.tree.css
+yui-compressor ./uncompressed/jquery.tree.css -o ./minified/jquery.tree.min.css
+
+if [ ! -e ./uncompressed/images ]; then mkdir ./uncompressed/images; fi
+cp ./src/css/images/*.gif ./uncompressed/images/
 
 if [ ! -e ./minified/images ]; then mkdir ./minified/images; fi
-cp ./src/images/*.gif ./minified/images/
+cp ./src/css/images/*.gif ./minified/images/
