ba3bb49bab29c085122b786cbb9510b357f35975
Valerio Galano <v.galano@daredevel.com>
2012-08-13 17:36:35 +0200

    New script builder with component selection feature

--- minify.sh
+++ minify.sh
@@ -1,14 +1,58 @@
 #!/bin/bash
 
+
+js="jquery.a.js jquery.tree.js";
+css="jquery.tree.css";
+
+#read -p "Do you want to include Ajax component? [Y/n]:"
+#if [ "$REPLY" != "n" ]; then
+#    js=$js" jquery.treeajax.js";
+#fi
+
+read -p "Do you want to include Checkbox component? [Y/n]:"
+if [ "$REPLY" != "n" ]; then
+    js=$js" jquery.treecheckbox.js";
+fi
+
+read -p "Do you want to include Collapse component? [Y/n]:"
+if [ "$REPLY" != "n" ]; then
+    js=$js" jquery.treecollapse.js";
+    css=$css" jquery.treecollapse.css";
+fi
+
+#read -p "Do you want to include Context Menu component? [Y/n]:"
+#if [ "$REPLY" != "n" ]; then
+#    js=$js" jquery.treecontextmenu.js";
+#    css=$css" jquery.treecontextmenu.css";
+#fi
+
+read -p "Do you want to include Drag&Drop component? [Y/n]:"
+if [ "$REPLY" != "n" ]; then
+    js=$js" jquery.treednd.js";
+    css=$css" jquery.treednd.css";
+fi
+
+read -p "Do you want to include Select component? [Y/n]:"
+if [ "$REPLY" != "n" ]; then
+    js=$js" jquery.treeselect.js";
+fi
+
+js=$js" jquery.z.js";
+
+
 if [ ! -e ./uncompressed ]; then mkdir ./uncompressed; fi
 if [ ! -e ./minified ]; then mkdir ./minified; fi
 
 if [ -e jquery.tree.js ]; then rm jquery.tree.js; fi
-cat ./src/js/*.js > ./uncompressed/jquery.tree.js
+cd ./src/js/
+cat $js > ../../uncompressed/jquery.tree.js
+cd ../../
 yui-compressor ./uncompressed/jquery.tree.js -o ./minified/jquery.tree.min.js
 
 if [ -e jquery.tree.css ]; then rm jquery.tree.css ; fi
-cat ./src/css/*.css > ./uncompressed/jquery.tree.css
+cd ./src/css/
+cat $css > ../../uncompressed/jquery.tree.css
+cd ../../
 yui-compressor ./uncompressed/jquery.tree.css -o ./minified/jquery.tree.min.css
 
 if [ ! -e ./uncompressed/images ]; then mkdir ./uncompressed/images; fi
